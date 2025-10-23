4bec327cf93df34b92891a78c70bf071828e523b
Blazehue <blazehue@example.com>
2025-10-23 23:46:21 +0530

    feat: create components barrel export

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Components index
+ * Central export point for all components
+ */
+
+export { BackgroundUpload } from './BackgroundUpload';
+export { FieldMapper } from './FieldMapper';
+export { FormEditor } from './FormEditor';
+export { PDFPreview } from './PDFPreview';
+export { PDFExporter } from './PDFExporter';
+export { Notifications } from './Notifications';
+export { ThemeProvider, useTheme } from './ThemeProvider';
+export { ThemeToggle } from './ThemeToggle';
